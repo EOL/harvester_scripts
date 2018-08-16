58a4ccbe143e687c495043f8e6e130a953c7aa1a
Youssef Eldakar <youssefeldakar@gmail.com>
2018-08-16 11:22:20 +0200

    Pass column indices as arguments to preproc.sh (was fix_dangling_refs.sh)

---
+++ preproc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# - Add missing parents with placeholder scientific name and rank
+# - Escape double-quotes
+
+if [ "$#" -ne 5 ]; then
+  echo "preproc.sh taxa.txt node_id_col parent_id_col scientific_name_col rank_col" >&2
+  exit 1
+fi
+
+join -t$'\t' -j1 -v2 <(tail -n+2 "$1"|awk -F'\t' "{print \$$2}"|sort) <(tail -n+2 "$1"|awk -F'\t' -v OFS='\t' "BEGIN{a[$4]=a[$5]=\"placeholder\"} \$$3!=\"\"{a[$2]=\$$3; o=a[1]; for(i=2;i<=NF;i++){o=o OFS a[i]} print o}"|sort -t$'\t' -k1b,1)|cat "$1" -|sed 's/"/\\"/g'
