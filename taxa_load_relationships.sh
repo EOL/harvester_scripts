9bb3e9945dc27220bd91be11541b017a236a3b63
MironaNazeer <mirona.gamil@bibalex.org>
2018-09-17 15:44:17 +0200

    load relations with GNode instead of Node

--- taxa_load_relationships.sh
+++ taxa_load_relationships.sh
@@ -8,7 +8,7 @@ fi
 cypher-shell <<EOF
 using periodic commit
 load csv from 'file:///$1' as line fieldterminator '\t'
-match (n:Node {node_id: line[$3], resource_id: $2})
-match (p:Node {node_id: line[$4], resource_id: $2})
+match (n:GNode {node_id: line[$3], resource_id: $2})
+match (p:GNode {node_id: line[$4], resource_id: $2})
 merge (p)-[:IS_PARENT_OF]->(n);
 EOF
