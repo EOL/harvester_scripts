ceb12702ae52b66ddf56a54064b34e616b7f807a
MironaNazeer <mirona.gamil@bibalex.org>
2018-11-12 14:34:26 +0200

    handle synonyms

--- taxa_generate_ids.sh
+++ taxa_generate_ids.sh
@@ -1,21 +1,61 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
-  echo "generate_ids.sh taxa.txt" >&2
+if [ "$#" -ne 7 ]; then
+# acceptedNameUsageIdCol accepted column number, -1 if column does not exist
+# taxStatusCol the status column number, -1 if column does not exist
+# parentCol the parent column number, -1 if column does not exist
+  echo "generate_ids.sh taxa.txt acceptedNameUsageIdCol taxStatusCol parentCol nodeIdCol hasHeader separator" >&2
   exit 1
 fi
 
-add=$(tail -n+2 "$1" | wc -l)
+add=""
+if [ "$6" = "true" ]; then
+    add=$(tail -n+2 "$1" | wc -l)
+else
+    add=$(tail -n+1 "$1" | wc -l)
+fi
 
 count=$(cypher-shell <<EOF | tail -n+2
 match (id:GlobalUniqueId) return id.count;
 EOF
 )
 
+
 cypher-shell <<EOF
 match (id:GlobalUniqueId) set id.count=id.count+$add;
 EOF
 
 tmpfile=$(mktemp)
-awk -F'\t' -v OFS='\t' -v count="$count" 'NR==1{print $0,"generated_auto_id"}NR>1{print $0,count+NR-1}' "$1" >"$tmpfile" && mv "$tmpfile" "$1"
+acceptedNameUsageIdCol=$2
+taxStatusCol=$3
+parentCol=$4
+nodeIdCol=$5
+hasHeader=$6
+
+awk -F$7 -v OFS=$7 -v count="$count" -v isAccepted="" -v acceptedParent="" -v acceptedNameUsageIdCol=$acceptedNameUsageIdCol -v  taxStatusCol=$taxStatusCol -v parentCol=$parentCol -v nodeIdCol=$nodeIdCol -v hasHeader=$hasHeader  'BEGIN{ split("accepted_accepted name_preffered_preffered name_provisionally accepted_provisionally accepted name_valid_valid name",parts,"_"); for (i in parts) vals[parts[i]]=""}
+{
+    if(NR==1 && hasHeader=="true"){
+    	print $0,"generated_auto_id","syn","accepted parent"
+    }
+    else{
+    	if(acceptedNameUsageIdCol>"-1") {
+    	    if($acceptedNameUsageIdCol=="" || $acceptedNameUsageIdCol==$nodeIdCol) {isAccepted="1";acceptedParent=""}#accepted
+    	    else {isAccepted="0";acceptedParent=$acceptedNameUsageIdCol}        
+    	}
+    	else{
+    	    if(taxStatusCol==-1){isAccepted="1";acceptedParent=""}#accepted
+    	    else{
+            	if($taxStatusCol=="") {isAccepted="1";acceptedParent=""}
+            	else if($taxStatusCol in vals) {isAccepted="1";acceptedParent=""}
+            	else{
+                	#check parent exist
+                	if($parentCol!=""){isAccepted="0";acceptedParent=$parentCol}
+                	else{isAccepted="0";acceptedParent=""} #parent not exist
+        	}
+    	    }
+       }
+       if(hasHeader=="true"){print $0,count+NR-1,isAccepted,acceptedParent}
+       else{print $0,count+NR,isAccepted,acceptedParent}
+    }
+}' "$1" >"$tmpfile" && mv "$tmpfile" "$1"
 chmod 777 "$1"
--- taxa_load_nodes_with_ids.sh
+++ taxa_load_nodes_with_ids.sh
@@ -6,17 +6,17 @@ if [ "$#" -ne 9 ]; then
 fi
 
 if [ "$8" = "true" ]; then
-if [ "$9" -ne -1]; then
+if [ "$9" != "-1" ]; then
 cypher-shell <<EOF
 using periodic commit 10000
 load csv from 'file:///$1' as line fieldterminator '\t' with line skip 1
+OPTIONAL match (n:GlobalUniqueId) where n.page_id < toInt(line[$9]) set n.page_id=toInt(line[$9])
 FOREACH(test IN CASE WHEN line [$7] IS NULL THEN [1] ELSE [] END | 
 	FOREACH(test IN CASE WHEN line [$9] IS NULL THEN [1] ELSE [] END |
 		create (n:GNode:Node:Root {node_id: line[$3], resource_id: $2, generated_auto_id: toInt(line[$6]), scientific_name: line[$4], rank: line[$5], created_at: timestamp(), updated_at: timestamp()})
 	)
 	FOREACH(test IN CASE WHEN line [$9] IS NOT NULL THEN [1] ELSE [] END |
 		create (n:GNode:Node:Root:Has_Page {node_id: line[$3], resource_id: $2, generated_auto_id: toInt(line[$6]), scientific_name: line[$4], rank: line[$5], page_id: toInt(line[$9]), created_at: timestamp(), updated_at: timestamp()})
-		match (n:GlobalUniqueId) where n.page_id < toInt(line[$9]) set n.page_id=toInt(line[$9])
 	)
 )
 FOREACH(test IN CASE WHEN line [$7] IS NOT NULL THEN [1] ELSE [] END | 
@@ -25,7 +25,6 @@ FOREACH(test IN CASE WHEN line [$7] IS NOT NULL THEN [1] ELSE [] END |
 	)
 	FOREACH(test IN CASE WHEN line [$9] IS NOT NULL THEN [1] ELSE [] END |
 		create (n:GNode:Node:Has_Page {node_id: line[$3], resource_id: $2, generated_auto_id: toInt(line[$6]), scientific_name: line[$4], rank: line[$5], page_id: toInt(line[$9]), created_at: timestamp(), updated_at: timestamp()})
-		match (n:GlobalUniqueId) where n.page_id < toInt(line[$9]) set n.page_id=toInt(line[$9])
 	)
 );
 EOF
